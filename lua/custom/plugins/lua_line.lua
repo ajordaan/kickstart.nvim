372f7d1673fbe488d9e39e934525a736aa882d44
Andrew <ajordaan23@gmail.com>
2024-04-08 11:16:02 +0100

    change: add custom file color for saved vs unsaved files

--- lua/custom/plugins/lua_line.lua
+++ lua/custom/plugins/lua_line.lua
@@ -13,6 +13,26 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local custom_fname = require('lualine.components.filename'):extend()
+    local highlight = require 'lualine.highlight'
+    local default_status_colors = { saved = '#228B22', modified = '#C70039' }
+
+    function custom_fname:init(options)
+      custom_fname.super.init(self, options)
+      self.status_colors = {
+        saved = highlight.create_component_highlight_group({ bg = default_status_colors.saved }, 'filename_status_saved', self.options),
+        modified = highlight.create_component_highlight_group({ bg = default_status_colors.modified }, 'filename_status_modified', self.options),
+      }
+      if self.options.color == nil then
+        self.options.color = ''
+      end
+    end
+
+    function custom_fname:update_status()
+      local data = custom_fname.super.update_status(self)
+      data = highlight.component_format_highlight(vim.bo.modified and self.status_colors.modified or self.status_colors.saved) .. data
+      return data
+    end
     vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
     vim.g.gitblame_date_format = '%r' -- relative format
     local git_blame = require 'gitblame'
@@ -28,9 +48,7 @@ return {
         },
         lualine_a = {
           {
-            'filename',
-            file_status = true, -- displays file status (readonly status, modified status)
-            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+            custom_fname,
           },
         },
         lualine_b = { { 'b:gitsigns_head', icon = '' }, { 'diff', source = diff_source } },
