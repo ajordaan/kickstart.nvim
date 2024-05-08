fc626914f995d833247fb6a5c2ed87c91eb9681a
Andrew Jordaan <andrew@judge.me>
2024-05-08 10:18:12 +0100

    add: text object custom keymaps

--- lua/custom/plugins/various_textobs.lua
+++ lua/custom/plugins/various_textobs.lua
@@ -3,18 +3,26 @@ return {
   lazy = false,
   opts = { useDefaultKeymaps = true },
   config = function()
-    vim.keymap.set({ 'o', 'x' }, 'aV', "<cmd>lua require('various-textobjs').value('inner')<CR>")
-    vim.keymap.set({ 'o', 'x' }, 'iV', "<cmd>lua require('various-textobjs').value('outer')<CR>")
+    vim.keymap.set({ 'o', 'x' }, 'aV', "<cmd>lua require('various-textobjs').value('outer')<CR>")
+    vim.keymap.set({ 'o', 'x' }, 'iV', "<cmd>lua require('various-textobjs').value('inner')<CR>")
 
-    vim.keymap.set({ 'o', 'x' }, 'aK', "<cmd>lua require('various-textobjs').key('inner')<CR>")
-    vim.keymap.set({ 'o', 'x' }, 'iK', "<cmd>lua require('various-textobjs').key('outer')<CR>")
+    vim.keymap.set({ 'o', 'x' }, 'aK', "<cmd>lua require('various-textobjs').key('outer')<CR>")
+    vim.keymap.set({ 'o', 'x' }, 'iK', "<cmd>lua require('various-textobjs').key('inner')<CR>")
 
     vim.keymap.set({ 'o', 'x' }, 'av', '<cmd>lua require("various-textobjs").subword("outer")<CR>')
     vim.keymap.set({ 'o', 'x' }, 'iv', '<cmd>lua require("various-textobjs").subword("inner")<CR>')
+
+    vim.keymap.set({ 'o', 'x' }, 'an', '<cmd>lua require("various-textobjs").number("outer")<CR>')
+    vim.keymap.set({ 'o', 'x' }, 'in', '<cmd>lua require("various-textobjs").number("inner")<CR>')
+
+    vim.keymap.set({ 'o', 'x' }, 'Q', '<cmd>lua require("various-textobjs").toNextQuotationMark()<CR>')
+    vim.keymap.set({ 'o', 'x' }, 'R', '<cmd>lua require("various-textobjs").restOfIndentation()<CR>')
+    vim.keymap.set({ 'o', 'x' }, 'r', '<cmd>lua require("various-textobjs").restOfParagraph()<CR>')
+    vim.keymap.set({ 'o', 'x' }, 'C', '<cmd>lua require("various-textobjs").toNextClosingBracket()<CR>')
   end,
 }
 
--- vim.keymap.set({ 'o', 'x' }, 'aV', "<cmd>lua require('various-textobjs').value('inner')<CR>")
+-- vim.keymap.set({ 'o', 'x' }, 'aV', "<cmd>lua require('various-textobjs').va123lue('inner')<CR>")
 -- vim.keymap.set({ 'o', 'x' }, 'iV', "<cmd>lua require('various-textobjs').value('outer')<CR>")
 --
 -- vim.keymap.set({ 'o', 'x' }, 'aK', "<cmd>lua require('various-textobjs').key('inner')<CR>")
